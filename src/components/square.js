c5b56e2c63c7771b223a8776b72dc824f29dd064
unimprobable <paul@unimprobable.com>
2022-05-21 21:44:02 -0400

    create starter game square component

---
+++ src/components/square.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">Click me</button>
+    )
+  }
+}
+
+export default Square
