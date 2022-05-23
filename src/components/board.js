7dc03d09230eb5cce034dcfe4cd4541fe56a9245
unimprobable <paul@unimprobable.com>
2022-05-22 21:42:56 -0400

    convert Board to functional component

--- src/components/board.js
+++ src/components/board.js
@@ -2,16 +2,14 @@ import * as React from "react"
 
 import Card from "./card"
 
-class Board extends React.Component {
-  render() {
-    const uniqueElementsArray = Array.from(Array(16),(e,i) => i + 1)
-  
-    return (
-      <div className="board">
-        {uniqueElementsArray.map((el,i) => <Card key={i} contents={el} />)}
-      </div>
-    )
-  }
+const Board = () => {
+  const uniqueElementsArray = Array.from(Array(16),(e,i) => i + 1)
+
+  return (
+    <div className="board">
+      {uniqueElementsArray.map((el,i) => <Card key={i} contents={el} />)}
+    </div>
+  )
 }
 
 export default Board
