880d0b8583bf94d7135d7b716befa2ec14889d5f
unimprobable <paul@unimprobable.com>
2022-05-23 00:42:45 -0400

    create handleClick for Card

--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,13 @@
 import * as React from "react"
 
-const Card = ({ contents, onClick, isFlipped, isInactive }) => {
+const Card = ({ contents, onClick, isFlipped, isInactive, index }) => {
+  const handleClick = () => {
+    !isFlipped && onClick(index);
+  }
   return (
     <button
       className="card visible"
-      onClick={onClick}
+      onClick={handleClick}
     >
       {contents}
     </button>
