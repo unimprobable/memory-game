99ad12a6cc6f9bc4e28c3279d6fabab068d588ed
unimprobable <paul@unimprobable.com>
2022-05-22 23:23:58 -0400

    add evaluate, handleCardClick, and state checks

--- src/components/game.js
+++ src/components/game.js
@@ -24,10 +24,55 @@ const numberArray = Array.from(Array(8),(e,i) => i + 1)
 
 const Game = () => {
   const [cards, setCards] = React.useState(shuffleCards(numberArray.concat(numberArray)));
+  const [openCards, setOpenCards] = React.useState([]);
+  const [clearedCards, setClearedCards] = React.useState({});
+  const [moves, setMoves] = React.useState(0);
+  const timeout = React.useRef(null);
 
   const handleRestart = () => {
     setCards(shuffleCards(numberArray.concat(numberArray)));
-  } 
+  };
+
+  // Check if both cards have the same number. If they do, mark them inactive.
+  const evaluate = () => {
+    const [first, second] = openCards;
+    if (cards[first].type === cards[second].type) {
+      setClearedCards((prev) => ({ ...prev, [cards[first]]: true }));
+      setOpenCards([]);
+      return;
+    }
+    // Flip cards after 1 sec
+    timeout.current = setTimeout(() => {
+      setOpenCards([]);
+    }, 1000);
+  };
+
+  const handleCardClick = (index) => {
+    // Have a maximum of 2 cards in array at once.
+    if (openCards.length === 1) {
+      setOpenCards((prev) => [...prev, index]);
+      // increase the moves once pair is opened
+      setMoves((moves) => moves + 1);
+    } else {
+      // If two cards are already open, cancel timeout set for flipping cards back
+      clearTimeout(timeout.current);
+      setOpenCards([index]);
+    }
+  };
+
+  React.useEffect(() => {
+    if (openCards.length === 2) {
+      setTimeout(evaluate, 1000);
+    }
+  }, [openCards]);
+
+  const checkIsFlipped = (index) => {
+    return openCards.includes(index);
+  };
+
+  const checkIsInactive = (card) => {
+    return Boolean(clearedCards[card]);
+  };
 
   return (
     <div className="game">
