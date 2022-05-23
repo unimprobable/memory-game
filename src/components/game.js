44b889e0aa331af63177cce10e0f55ccbb139a11
unimprobable <paul@unimprobable.com>
2022-05-22 21:33:26 -0400

    convert Game to functional component

--- src/components/game.js
+++ src/components/game.js
@@ -2,20 +2,18 @@ import * as React from "react";
 
 import Board from "./board";
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="info-row">
-          <button className="new-game-button">New Game</button>
-          <div className="score">Score: 100</div>
-        </div>
-        <div className="game-board">
-          <Board />
-        </div>
+const Game = () => {
+  return (
+    <div className="game">
+      <div className="info-row">
+        <button className="new-game-button">New Game</button>
+        <div className="score">Score: 100</div>
       </div>
-    )
-  }
+      <div className="game-board">
+        <Board />
+      </div>
+    </div>
+  )
 }
 
 export default Game
