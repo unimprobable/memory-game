009633529fab61cd61eb93087fe2489deb801223
unimprobable <paul@unimprobable.com>
2022-05-22 22:33:03 -0400

    add swap and shuffleCards functions

--- src/components/game.js
+++ src/components/game.js
@@ -2,6 +2,23 @@ import * as React from "react";
 
 import Board from "./board";
 
+// Fisher Yates Shuffle
+function swap(array, i, j) {
+  const temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+function shuffleCards(array) {
+  const length = array.length;
+  for (let i = length; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * i);
+    const currentIndex = i - 1;
+    swap(array, currentIndex, randomIndex);
+  }
+  return array;
+}
+
 const Game = () => {
   const [cards, setCards] = React.useState(Array.from(Array(16),(e,i) => i + 1));
 
