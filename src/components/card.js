dc9344fdf4a931bb90e2180313a1c883778798c8
unimprobable <paul@unimprobable.com>
2022-05-23 01:05:46 -0400

    add classNames func

--- src/components/card.js
+++ src/components/card.js
@@ -1,12 +1,20 @@
 import * as React from "react"
 
+const classNames = (...classes) => {
+  return classes.filter(Boolean).join(" ");
+};
+
 const Card = ({ contents, onClick, isFlipped, isInactive, index }) => {
   const handleClick = () => {
     !isFlipped && onClick(index);
   }
+  
   return (
     <button
-      className="card visible"
+      className={classNames(
+        "card",
+        isInactive ? "matched" : "not-matched"
+      )}
       onClick={handleClick}
     >
       {isFlipped && contents}
